a2a868b67f9588431a9b98f9726032fe938a6d17
jprecht <jonas@precht.de>
2015-12-01 18:50:47 +0100

    Updated Ingredient to work + old Recipe creator that needs a rework

--- ecatering/src/main/java/org/tudresden/ecatering/frontend/KitchenController.java
+++ ecatering/src/main/java/org/tudresden/ecatering/frontend/KitchenController.java
@@ -3,6 +3,10 @@ package org.tudresden.ecatering.frontend;
 import static org.salespointframework.core.Currencies.EURO;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -15,11 +19,14 @@ import org.tudresden.ecatering.model.kitchen.Meal;
 import org.tudresden.ecatering.model.kitchen.MealRepository;
 import org.tudresden.ecatering.model.kitchen.MealType;
 import org.tudresden.ecatering.model.kitchen.MenuRepository;
+import org.tudresden.ecatering.model.kitchen.Recipe;
 import org.tudresden.ecatering.model.kitchen.RecipeRepository;
 import org.tudresden.ecatering.model.stock.Ingredient;
 import org.tudresden.ecatering.model.stock.IngredientRepository;
 import org.tudresden.ecatering.model.stock.StockManager;
 import org.javamoney.moneta.Money;
+import org.salespointframework.catalog.Product;
+import org.salespointframework.quantity.Metric;
 import org.salespointframework.quantity.Quantity;
 
 
@@ -52,12 +59,49 @@ class KitchenController {
 	}
 	
 	@RequestMapping(value = "/addIngredient", method = RequestMethod.POST)
-	public String addIngredient(@RequestParam("name") String name, @RequestParam("quantity") int quantity,@RequestParam("price") double price,@RequestParam("DD") int day,@RequestParam("MM") int month,@RequestParam("YYYY") int year) {
+	public String addIngredient(@RequestParam("name") String name, @RequestParam("quantity") double quantity,@RequestParam("price") double price, @RequestParam("metric") String metric,@RequestParam("DD") String dayInput,@RequestParam("MM") String monthInput,@RequestParam("YYYY") String yearInput) {
 		//Actually create the Ingredient
-		Quantity menge = Quantity.of(quantity);
-		Ingredient ingredient = StockManager.createIngredient(name,Money.of(price, EURO),menge,LocalDate.of(year, month, day));
-		//Need to find out how we can save the date right
-		stockManager.saveIngredient(ingredient);
+		//Quantity menge = Quantity.of(quantity);
+		
+		int month = Integer.parseInt(monthInput);
+		int year = Integer.parseInt(yearInput);
+		int day = Integer.parseInt(dayInput);
+		
+		for(Metric m : Metric.values())
+	    {
+	      //System.out.println(m.name());
+	      if(m.name().contains(metric))
+	      {
+	        System.out.println("Metric: "+m.name());
+	        Quantity menge = Quantity.of(quantity, m);
+	        
+	        if(day==0){
+	        	
+	        	Ingredient ingredient = StockManager.createIngredient(name,Money.of(price, EURO),menge);
+	        	stockManager.saveIngredient(ingredient);
+	        	System.out.println("Ohne Datum");
+	        }else{
+	        int i=0;
+	        for(Month mo : Month.values())
+	        {
+	        	i++;
+	          //System.out.println(mo.name());
+	          if(i==month)
+	          {
+	            System.out.println("Monat: "+mo.name());
+	            Month m1 = mo;
+	            
+	            Ingredient ingredient = StockManager.createIngredient(name,Money.of(price, EURO),menge,LocalDate.of(year, m1, day));
+				//Need to find out how we can save the date right
+				stockManager.saveIngredient(ingredient);
+	          	}
+	          }
+	        }
+	      }
+	    }
+		
+		
+		
 		return "createIngredient";
 	}
 	
@@ -88,6 +132,51 @@ class KitchenController {
 	public String listMeals(ModelMap modelMap) {
 		modelMap.addAttribute("allMeals", kitchenManager.findAllMeals());
 		return "listMeals";
+	}
+	
+	@RequestMapping("/createRecipe")
+	public String createRecipe(ModelMap modelMap){
+		modelMap.addAttribute("allMeals", kitchenManager.findAllMeals());
+		return "createRecipe";
+	}
+	
+	@RequestMapping(value = "/saveRecipe", method = RequestMethod.POST)
+	public String saveRecipe(@RequestParam("name") String name, @RequestParam("meal") String meal, @RequestParam("ing") ArrayList<String> ing, @RequestParam("quan") ArrayList<Integer> quan) {
+		//Check if I can send arrays to java if the namefields are called array[]
+		
+//		if(ing.size()!=quan.size()){
+//			
+//		}
+		
+//		List<Ingredient> inList = new ArrayList<Ingredient>();
+//		System.out.println("Max Ing"+ing.size()+"Max Quan"+quan.size());
+//		for(int i=0; i < ing.size(); i++){
+//			
+//		System.out.println("Ingreidents"+ing.get(i)+" Quantity"+quan.get(i));
+//
+//		
+////		Product p1 = new Product(ing.get(i),Money.of(0.00, EURO));
+////		Quantity q1 = Quantity.of(quan.get(i));
+////		LocalDateTime expDate1 = LocalDateTime.of(2015, 12, 24, 0, 0);
+//		
+//		//Ingredient in1 = new Ingredient(p1,q1,expDate1);
+//		
+//		inList.add(new Ingredient(new Product(ing.get(i),Money.of(0.00, EURO)),Quantity.of(quan.get(i)),LocalDateTime.of(2015, 12, 24, 0, 0)));
+//
+//		}
+//		
+//	
+//				Iterable<Meal> pizzaMeals = kitchenManager.findMealsByName(meal);
+//				Meal meals = pizzaMeals.iterator().next();
+//				Recipe recipe = kitchenManager.createRecipe(name, inList, meals.getIdentifier());
+//				kitchenManager.saveRecipe(recipe);
+		
+
+		
+						
+		
+		
+		return "redirect:/createRecipe";
 	}
 
 }
